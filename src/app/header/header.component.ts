c5e882b1fbacf3d38139d1d7a686c7a2b68c9aca
antonnymello <antonny.mello@gmail.com>
2020-09-08 10:59:40 -0300

    Top-nav added

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent implements OnInit {
+  title = `Lead me a Movie`;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
